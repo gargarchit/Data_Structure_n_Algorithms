32b8a557730f156f96b9a4281bc0aa9d8eb857d9
Varsha Reddy <38997871+varshakancham@users.noreply.github.com>
2020-10-03 14:31:03 +0530

    Create Arranging Coins.java

---
+++ Competitive_Programming/Arranging Coins.java
@@ -0,0 +1,44 @@
+/*You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+
+Given n, find the total number of full staircase rows that can be formed.
+
+n is a non-negative integer and fits within the range of a 32-bit signed integer.
+
+Example 1:
+
+n = 5
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤
+
+Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+n = 8
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤ ¤
+¤ ¤
+
+Because the 4th row is incomplete, we return 3.*/
+class Solution {
+    public int arrangeCoins(int n) {
+        if(n==0)
+            return 0;
+        
+       int count=0;
+        for(int i=1;i<=n;i++){
+            if(n>=i){
+                n=n-i;
+                count++;
+            }
+            else
+                break;
+        }
+        return count;
+    }
+}
