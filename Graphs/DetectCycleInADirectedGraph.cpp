02c316d710567543511d867b5671a4e583e04d1b
Sejal Chowdhury <45739778+excess30@users.noreply.github.com>
2020-10-16 18:09:57 +0545

    Update DetectCycleInADirectedGraph.cpp

--- Graphs/DetectCycleInADirectedGraph.cpp
+++ Graphs/DetectCycleInADirectedGraph.cpp
@@ -1,12 +1,37 @@
-// { Driver Code Starts
+//Given a Directed Graph. Check whether it contains any cycle or not.
+
+//Input: The first line of the input contains an integer 'T' denoting the number of test cases. Then 'T' test cases follow. Each test case consists of two lines. Description of testcases is as follows: The First line of each test case contains two integers 'N' and 'M'  which denotes the no of vertices and no of edges respectively. The Second line of each test case contains 'M'  space separated pairs u and v denoting that there is an uni-directed  edge from u to v .
+
+//Output:
+//The method should return 1 if there is a cycle else it should return 0.
+
+//User task:
+//You don't need to read input or print anything. Your task is to complete the function isCyclic which takes the Graph and the number of vertices and inputs and returns true if the given directed graph contains a cycle. Else, it returns false.
+
+//Expected Time Complexity: O(V + E).
+//Expected Auxiliary Space: O(V).
+
+//Constraints:
+//1 <= T <= 1000
+//1<= N,M <= 1000
+//0 <= u,v <= N-1
+
+//Example:
+//Input:
+//3
+//2 2
+//0 1 0 0
+//4 3
+//0 1 1 2 2 3
+//4 3
+//0 1 2 3 3 2
+//Output:
+//1
+//0
+//1
+
 #include <bits/stdc++.h>
 using namespace std;
- // } Driver Code Ends
- 
-/*  Function to check if the given graph contains cycle
-*   V: number of vertices
-*   adj[]: representation of graph
-*/
 bool cyclic(int i, int V, vector<int> adj[],bool visited[], bool inTheCall[])
 {
     visited[i]=true;
@@ -42,7 +67,7 @@ bool isCyclic(int V, vector<int> adj[])
     }
     return false;
 }
-// { Driver Code Starts.
+
 int main() 
 {
 	int t;
@@ -58,4 +83,4 @@ int main()
 	    }
 	    cout << isCyclic(v, adj) << endl;   
 	return 0;
-}  // } Driver Code Ends
+}  
